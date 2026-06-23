3301f6ed0a6fdd93e54f1ad6a7eb0c906188420f
Aaroen <aroen11269049@gmail.com>
2026-06-23 10:57:00 +0800

    Reset gestures after canceling stale reader swipe

--- web/src/composables/useAppPointerGestureInteractions.ts
+++ web/src/composables/useAppPointerGestureInteractions.ts
@@ -19,6 +19,7 @@ export function useAppPointerGestureInteractions(options: AppPointerGestureInter
     }
     if (options.touch.readerBackSwipeTrackingActive.value) {
       options.touch.cancelBackSwipe()
+      options.touch.resetGestureTracking()
     }
   }
 
